c478c7402e407b1cbabb2bc71294c46586f964db
tanayarun <ankulwartanay@gmail.com>
2026-03-07 20:18:05 +0530

    watchCmd Flag

--- cmd/watch.go
+++ cmd/watch.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var watchCmd = &cobra.Command{
+	Use:   "watch",
+	Short: "To wathch the orderbook of symbol",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		symbol, err := cmd.Flags().GetString("symbol")
+		if err != nil {
+			return err
+		}
+		fmt.Println(symbol)
+		return nil
+	},
+}
+
+func init() {
+	watchCmd.Flags().StringP("symbol", "s", "", "Trading pair e.g. BTCUSDT")
+}
