22779f7855dcf25f649281c8b867a7de79ab8706
tanayarun <ankulwartanay@gmail.com>
2026-03-08 15:29:49 +0530

    working version

--- cmd/portfolio.go
+++ cmd/portfolio.go
@@ -1,2 +1,12 @@
 // Package cmd is used to get data from api
 package cmd
+
+import "github.com/spf13/cobra"
+
+var portfolioCmd = &cobra.Command{
+	Use: "portfolio",
+	Short: "see your portfolio",
+}
+
+func init() {
+}
--- cmd/tickers.go
+++ cmd/tickers.go
@@ -1,1 +1,8 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+var tickersCmd = &cobra.Command{}
+
+func init() {
+}
