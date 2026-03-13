e855b2d7d87d897f3a81cf5ca3c97d609505ec09
tanayarun <ankulwartanay@gmail.com>
2026-03-13 10:44:53 +0530

    Established Client(apikey, secretkey)

--- internal/binance/client.go
+++ internal/binance/client.go
@@ -1,1 +1,14 @@
+// Package binance is used to establish connection with binance
 package binance
+
+type Client struct {
+	apiKey    string
+	secretKey string
+}
+
+func NewClient(apiKey, secretKey string) *Client {
+	return &Client{
+		apiKey : apiKey,
+	secretKey : secretKey,
+	}
+}
