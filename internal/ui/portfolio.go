0a96b171151871654908a6d75c5c0274d76f01e0
tanayarun <ankulwartanay@gmail.com>
2026-03-16 15:09:41 +0530

    Bubble tea initial structure

--- internal/ui/portfolio.go
+++ internal/ui/portfolio.go
@@ -1,1 +1,26 @@
 package ui
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/tanayarun/Binance-TUI/internal/binance"
+)
+
+type Model struct {
+	balances []binance.Balance
+}
+
+func NewPortfolioModel() Model {
+	return Model{}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return nil, nil
+}
+
+func (m Model) View() string {
+	return ""
+}
