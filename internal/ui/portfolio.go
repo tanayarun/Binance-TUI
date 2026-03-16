e99122fda97fb19396d9c2e36e5f6acf57a4dd88
tanayarun <ankulwartanay@gmail.com>
2026-03-16 18:38:50 +0530

    fetchBalance tea

--- internal/ui/portfolio.go
+++ internal/ui/portfolio.go
@@ -6,21 +6,41 @@ import (
 )
 
 type Model struct {
+	client   *binance.Client
 	balances []binance.Balance
 }
 
-func NewPortfolioModel() Model {
-	return Model{}
+type balancesLoadedMsg struct {
+	balances []binance.Balance
+}
+
+type errMsg struct {
+	err error
+}
+
+func NewPortfolioModel(client *binance.Client) Model {
+	return Model{client: client}
 }
 
 func (m Model) Init() tea.Cmd {
-	return nil
+	return m.fetchBalances()
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return nil, nil
+	return m, nil
 }
 
 func (m Model) View() string {
 	return ""
+}
+
+func (m Model) fetchBalances() tea.Cmd {
+	return func() tea.Msg {
+		result, err := m.client.GetBalances()
+		if err != nil {
+			return errMsg{err: err}
+		}
+
+		return balancesLoadedMsg{balances: result}
+	}
 }
