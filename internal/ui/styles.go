872c724688073da062ad26de82b83fce7102cf12
tanayarun <ankulwartanay@gmail.com>
2026-03-19 14:22:14 +0530

    Defined basic styles

--- internal/ui/styles.go
+++ internal/ui/styles.go
@@ -1,1 +1,9 @@
 package ui
+
+import "github.com/charmbracelet/lipgloss"
+
+var StyleHeader = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#ffd700"))
+
+var StyleGreen = lipgloss.NewStyle().Foreground(lipgloss.Color("#00ff88"))
+
+var StyleGray = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262"))
