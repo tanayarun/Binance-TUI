58c2143cc3be91218ab4f423f914b553e7635597
tanayarun <ankulwartanay@gmail.com>
2026-03-21 19:48:08 +0530

    showing best bid/ask

--- internal/binance/stream.go
+++ internal/binance/stream.go
@@ -2,11 +2,18 @@ package binance
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 
 	"github.com/gorilla/websocket"
 )
+
+type DepthUpdate struct {
+	Symbol string     `json:"s"`
+	Bids   [][]string `json:"b"`
+	Asks   [][]string `json:"a"`
+}
 
 func ConnectOrderbook(symbol string) (*websocket.Conn, error) {
 	url := fmt.Sprintf("wss://demo-stream.binance.com:9443/ws/%s@depth", strings.ToLower(symbol))
@@ -25,6 +32,14 @@ func ReadOrderbook(conn *websocket.Conn) error {
 			return err
 		}
 
-		fmt.Println(string(p))
+		var update DepthUpdate
+		err = json.Unmarshal(p, &update)
+		if err != nil {
+			return err
+		}
+
+		if len(update.Bids) > 0 && len(update.Asks) > 0 {
+			fmt.Printf("Best Bid: %s | Best Ask: %s\n", update.Bids[0][0], update.Asks[0][0])
+		}
 	}
 }
