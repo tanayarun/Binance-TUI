3b0247fa05a16ede96e11a2e83e2fabc5b70bbbc
tanayarun <ankulwartanay@gmail.com>
2026-03-23 21:15:05 +0530

    stylered

--- internal/ui/styles.go
+++ internal/ui/styles.go
@@ -7,3 +7,5 @@ var StyleHeader = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#ffd
 var StyleGreen = lipgloss.NewStyle().Foreground(lipgloss.Color("#00ff88")).Width(0)
 
 var StyleGray = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262"))
+
+var StyleRed = lipgloss.NewStyle().Foreground(lipgloss.Color("#ff4d6d"))
